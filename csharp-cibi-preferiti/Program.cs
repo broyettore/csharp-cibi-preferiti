e13aee82e3fc4e32611fc9b0ada1f3b7ff9b79ad
broyettore <broyettore@gmail.com>
2023-09-05 14:58:25 +0200

    task done with bonus

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] favMeals = { "pizza", "sushi", "bbq", "burger", "tacos", "seafood"};
+
+Console.WriteLine($"the number of your favourite meals is: {favMeals.Length}.");
+
+for (int i = 0; i < favMeals.Length; i++)
+{
+    Console.WriteLine($"{(i + 1)} - {favMeals[i]}");
+}
+
+Console.WriteLine($"your absolute favourite meal is: {favMeals[0]}.");
+
+Console.WriteLine($"your least favourite meal is: {favMeals[favMeals.Length - 1]}.");
+
+//Bonus  
+
+if (favMeals.Length % 2 == 0)
+{
+    Console.WriteLine($"your average favourite meals are: {favMeals[(favMeals.Length / 2) - 1]}, {favMeals[favMeals.Length / 2]}.");
+}
+else
+{
+    Console.WriteLine($"your average favourite meal is: {favMeals[favMeals.Length / 2]}.");
+}
